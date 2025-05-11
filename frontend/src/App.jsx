18567d4f5d99420b08e6a22582978ea53d4e46da
Harpalsinh Sindhav <88263558+harpalll@users.noreply.github.com>
2025-05-11 18:59:57 +0530

    Frontend Init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-amber-400 min-w-full min-h-[100vh] flex justify-center items-center">
+      <h1 className="text-7xl text-white font-bold">Welcome to Mattermind</h1>
+    </div>
+  );
+};
+
+export { App };
